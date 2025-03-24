9df50b473200bd658970c1dd7697b2308fb4c5e0
sua <dssua0714@naver.com>
2025-03-24 16:28:30 +0900

    모델 생성 및 처리

--- board/models.py
+++ board/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# dev_2
+class Question(models.Model) : 
+    title = content = models.CharField(max_length=200)      # 글 제목
+    content = models.TextField()                            # 글 내용
+    create_date = models.DateTimeField()                    # 작성 시간
+
+
+    def __str__(self) : 
+        return self.title
+    
+
+class Answer(models.Model) : 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateField()
+
+    def __str__(self) : 
+        return f"Answer to : {self.question.title}"
+    
+    
--- memo.md
+++ memo.md
@@ -13,3 +13,18 @@
 
 ## 앱 등록하기 (settings.py)
     config/settings.py에서 INSTALLED_APPS에 'board', 추가
+
+##  모델을 생성하거나 변경할 경우
+    python manage.py makemigrations
+    python manage.py migrate
+    
+    => db.sqlite3 파일에 board_question / board_answer 테이블이 생김
+
+## 확인해보고 싶으면
+    python manage.py shell
+
+    from board.models import Question, Answer
+    Question.objects.all()
+    Answer.objects.all()
+
+    => 아무것도 없다면 <QuerySet []> 출력
