a2d01cf6a3f96d492a242fd53925b7de179cc1f5
sua <dssua0714@naver.com>
2025-03-25 17:30:09 +0900

    페이징 (페이지네이션)

--- board/views.py
+++ board/views.py
@@ -2,15 +2,43 @@ from django.utils import timezone
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AnswerForm, QuestionForm
 from .models import Question
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 # dev_3
 def question_list(request) : 
+    """질문 목록 (페이징 적용)"""
+
+    # ?page=1
+    page = request.GET.get("page", "1")  # URL에서 ?page=1 값을 가져옴
+
     question_list = Question.objects.order_by("-create_date")
 
-    context = {"question_list" : question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)     # 해당 페이지에 해당하는 데이터 가져옴
+    
+    # 페이지네이션 범위 계산 (5개씩 끊어서)
+    current_page = page_obj.number  # 현재 페이지 번호
+    total_pages = paginator.num_pages  # 전체 페이지 개수
+    page_range = 5  # 한 번에 보여줄 페이지 개수 (5개씩)
 
+    start_page = ((current_page - 1) // page_range) * page_range + 1
+    end_page = start_page + page_range - 1
+    if end_page > total_pages:
+        end_page = total_pages
+
+    context = {
+        "question_list": page_obj,
+        "page_range": range(start_page, end_page + 1),  # 5개씩 끊어진 페이지 목록
+        "has_previous": page_obj.has_previous(),
+        "has_next": page_obj.has_next(),
+        "previous_page_number": page_obj.previous_page_number() if page_obj.has_previous() else None,
+        "next_page_number": page_obj.next_page_number() if page_obj.has_next() else None,
+        "start_page": start_page,
+        "end_page": end_page,
+        "total_pages": total_pages,
+    }
     return render(request, "board/question_list.html", context)
 
 
@@ -75,3 +103,5 @@ def answer_create(request, question_id):
 
     context = {'question': question, 'form': form}
     return render(request, 'board/question_detail.html', context)
+
+
